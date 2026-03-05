a7e3734007dfc302f04da57f794e493c68affcf0
Coslly <2268455705@qq.com>
2026-03-05 13:23:42 +0900

    Game update (14138)

--- output/soundsystem_dll.cs
+++ output/soundsystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-02-24 01:42:29.356100200 UTC
+// 2026-03-05 04:22:33.269449500 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: soundsystem.dll
